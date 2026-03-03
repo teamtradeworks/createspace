5ab9da8247ad9c4deb2a642472b6f654782b78dd
Dave New <sanodn@gmail.com>
2026-03-03 17:19:11 +0200

    Add privacy policy content

--- storefront/src/app/sitemap.ts
+++ storefront/src/app/sitemap.ts
@@ -74,7 +74,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
     {
       url: `${BASE_URL}/privacy`,
-      lastModified: new Date("2025-06-01"),
+      lastModified: new Date("2026-03-03"),
       changeFrequency: "yearly",
       priority: 0.3,
     },
